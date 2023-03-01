85275f8818dae16d93a020e56684b5a4329005ea
amequi12 <58491295+amequi12@users.noreply.github.com>
2023-03-01 18:38:38 +0500

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+feather.replace();
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.set('section.scrollable', {position: 'absolute'});
+
+// gsap.to('section.scrollable', {
+//   yPercent: -100,
+//   stagger: 2,
+//   scrollTrigger: {
+//     trigger: '.container-fluid',
+//     start: 'top top',
+//     scrub: true,
+//     pin: true,
+//     ease: 'in.out(2)'
+//   }
+// })
+
+// $(document).ready(function() {
+//   $(document).scroll(function() {
+//     if ($(this).scrollTop() < 100) {
+//       $('.navbar').removeClass('light');
+//       $('.navbar').addClass('dark');
+//     }
+//     if ($(this).scrollTop() >= 100 && $(this).scrollTop() < 450) {
+//       $('.nav-btn').removeClass('hidden');
+//       $('.navbar').removeClass('dark');
+//       $('.navbar').addClass('light');
+//     }
+//     if ($(this).scrollTop() >= 450 && $(this).scrollTop() < 650) {
+//       $('.navbar').removeClass('light');
+//       $('.navbar').addClass('dark');
+//     }
+//     if ($(this).scrollTop() >= 650 && $(this).scrollTop() < 850) {
+//       $('.navbar').removeClass('dark');
+//       $('.navbar').addClass('light');
+//     }
+//     if ($(this).scrollTop() >= 850) {
+//       $('.navbar').removeClass('light');
+//       $('.navbar').addClass('dark');
+//     }
+//   });
+// });
